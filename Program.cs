94ad170687bdcb0f31059f12276ac55304dc82e4
karankharole <karankharole2000@gmail.com>
2023-01-18 22:20:37 +0530

    create stack and push elements in it

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StackAndQueueNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
